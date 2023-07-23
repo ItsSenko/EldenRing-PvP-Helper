20f2b9ebbc045f7de8e0442eb9194b26185c2823
Senko <68884768+ItsSenko@users.noreply.github.com>
2023-07-23 04:14:41 -0400

    Added Console and Command Boilerplate.

---
+++ PvP Helper NewUI/PvPHelper/Console/CommandBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PvPHelper.Console
+{
+    public abstract class CommandBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CommandString { get; set; }
+        public bool RequireParams { get; set; }
+        public bool HasParams { get; set; }
+        public string[] RequiresParamsString { get; set; }
+
+        protected virtual void OnTriggerCommand() { }
+        protected virtual void OnTriggerCommandWithParameters(List<string> parameters) { }
+
+        public void Trigger()
+        {
+            OnTriggerCommand();
+        }
+        public void TriggerWithParameters(List<string> parameters)
+        {
+            OnTriggerCommandWithParameters(parameters);
+        }
+    }
+}
